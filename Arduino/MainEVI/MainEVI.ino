90c06e291fe7afbd754db05fcaf93bebe9c33bc7
kmw3kx <bobkammauff@gmail.com>
2024-12-07 01:53:00 -0500

    Started Arduino Code

---
+++ Arduino/MainEVI/MainEVI.ino
@@ -0,0 +1,19 @@
+// Bob Kammauff (kmw3kx)
+// Arduino Uno EVI
+//12-7-24
+
+/*
+Goal: Build a USB Wind-Controller using an Arduino Uno
+
+I want to use some object-oriented programming to make this cleaner
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
